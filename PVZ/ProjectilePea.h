3da097a06b3fbb6b6dd9ad442b4b5e9670b26db5
lienji2697085140-eng <lienji2697085140@gmail.com>
2025-11-01 02:04:03 +0800

    修改了豌豆射手的逻辑，使其可以一次发射多颗子弹
    Update ProjectilePea.cpp
    
    Update PeaShooterSingle.cpp
    
    修改了豌豆射手的功能，使其一次能发出四颗豌豆子弹
    
    Update PeaShooterSingle.h

--- PVZ/ProjectilePea.h
+++ PVZ/ProjectilePea.h
@@ -3,16 +3,26 @@
 #include "Ejects.h"
 
 class ProjectilePea : public Ejects {
-  DECLARE_DYNCREATE(ProjectilePea)
+    DECLARE_DYNCREATE(ProjectilePea)
 
 public:
-  // 移动速度
-  static constexpr int step = 7;
-  static constexpr int hurt = 20;
+    static constexpr int step = 7;
+    static constexpr int hurt = 20;
+
+private:
+    int penetrationCount;
+    int hitCount; // 记录击中次数
 
 public:
-  ProjectilePea(CRuntimeClass* msg = RUNTIME_CLASS(ProjectilePea),
-    int h = ProjectilePea::hurt);
+    ProjectilePea(CRuntimeClass* msg = RUNTIME_CLASS(ProjectilePea),
+        int h = ProjectilePea::hurt);
 
-  virtual void move();
+    virtual void move();
+
+    void setPenetration(int count) {
+        if (count > 0) penetrationCount = count;
+    }
+    int getPenetration() const { return penetrationCount; }
+    int getHitCount() const { return hitCount; }
+    bool isActive() const { return penetrationCount > 0; }
 };
