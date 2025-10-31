159d76825241a53e21245ee45362a54db0b50464
lienji2697085140-eng <lienji2697085140@gmail.com>
2025-11-01 02:04:12 +0800

    Update ProjectilePea.cpp

--- PVZ/ProjectilePea.cpp
+++ PVZ/ProjectilePea.cpp
@@ -3,6 +3,9 @@
 IMPLEMENT_DYNCREATE(ProjectilePea, Ejects)
 
 ProjectilePea::ProjectilePea(CRuntimeClass* msg, int hurt)
-  : Ejects(msg, hurt) {}
+    : Ejects(msg, hurt), penetrationCount(3), hitCount(0) {
+}
 
-void ProjectilePea::move() { leftX += ProjectilePea::step; }
+void ProjectilePea::move() {
+    leftX += ProjectilePea::step;
+}
