1cc71ad9cad36ba95e2b7603efb743f156530f57
WuzuSama <wuzudaren@163.com>
2025-11-04 01:16:29 +0800

    添加了铁桶僵尸类文件

---
+++ PVZ/BucketHeadZombie.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "stdafx.h"
+#include "Zombie.h"
+
+class BucketHeadZombie : public Zombie {
+    DECLARE_DYNCREATE(BucketHeadZombie)
+
+private:
+    static constexpr int HP = 540;  // 铁桶僵尸有更高的生命值
+    static constexpr double MOVESPEED = 0.00018;  // 比普通僵尸稍慢
+    static constexpr int HURT = 60;  // 伤害值与普通僵尸相同
+
+public:
+    BucketHeadZombie(CRuntimeClass* classMsg = RUNTIME_CLASS(BucketHeadZombie),
+        int hp = HP,
+        double moveSpeed = MOVESPEED,
+        int hurt = HURT);
+};
